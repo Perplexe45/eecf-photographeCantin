71d80217a2e22af80c622a3c3f29b7ff3eedf175
Perplexe45 <aasselin@free.fr>
2023-09-28 10:08:07 +0200

    Modif suite à une erreur lors du build

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,9 +5,6 @@ import { Inter } from 'next/font/google';
 
 import Head from 'next/head';
 //import './globals.css';
-import { styles } from "./../../styles/container.css";
-
-
 
 const inter = Inter({ subsets: ['latin'] })
 
